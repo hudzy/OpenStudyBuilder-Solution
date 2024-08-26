02a958c96cb8040830f8dd808f3eb0fc6d5c6bfc
HudsonHu <hudson.hu@sanofi.com>
2024-08-26 08:59:21 +0800

    update on 2024-08-26T00:59:21Z

--- build-push-image.sh
+++ build-push-image.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+set -euo pipefail
+# set -x
+cd $(dirname $0)
 
 # Path to your docker-compose.yaml file
-COMPOSE_FILE="docker-compose.yaml"
+COMPOSE_FILE="docker-compose-build.yaml"
+
+# Build images
+docker compose -f $COMPOSE_FILE build
 
 # Extract image names using sed and awk
 IMAGES=$(sed -n 's/^\s*image:\s*\(.*\)/\1/p' "$COMPOSE_FILE" | awk '{print $1}' | grep hudzy)
@@ -11,4 +17,3 @@ for IMAGE in $IMAGES; do
   echo "Pushing image: $IMAGE"
   docker push "$IMAGE"
 done
-
